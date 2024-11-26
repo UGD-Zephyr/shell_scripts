e3026109faffef2fd8d1227b8f8a99641437f837
Zephyr <78292553+UGD-Zephyr@users.noreply.github.com>
2024-11-26 14:53:03 +0100

    adding files

--- GitHubActivationScript.ps1
+++ GitHubActivationScript.ps1
@@ -1,6 +1,6 @@
 # Programmer:       Per Stoor
 # Date:             2024-07-25
-# Last changed:     2024-07-25
+# Last changed:     2024-11-26
 # Type of script:   PowerShell script to activate gh auth login and ssh-key for working with GitHub.
 
 Write-Output "1. Log in to GitHub. "
@@ -12,7 +12,7 @@ $UserChoice = Read-Host
     if ($UserChoice -eq 1) {
         Write-Host "logging in to GitHub with [gh auth login]"
         Write-Host "Please wait..."
-        Get-Content "[PATH TO .txt file with token]" | gh auth login --with-token 
+        Get-Content "[PATH TO TOKEN]" | gh auth login --with-token 
         Write-Host "Login successful!"
     }
     elseif ($UserChoice -eq 2) {
@@ -21,7 +21,13 @@ $UserChoice = Read-Host
         gh auth logout
     }
     elseif ($UserChoice -eq 3) {
-        ssh-add "[PATH TO SSH key]"
+        if ((Get-Service ssh-agent).Status -eq 'Running') {
+            Start-Process ssh-add [PATH TO SSH KEY]
+        }
+        else {
+            Start-Service ssh-agent
+            Start-Process ssh-add [PATH TO SSH KEY]
+        }
      }
     else {
         Write-Host "You did not enter 1, 2 or 3..."
